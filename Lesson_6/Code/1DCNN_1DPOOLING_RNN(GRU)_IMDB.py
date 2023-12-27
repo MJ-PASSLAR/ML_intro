83c8bd36b964134628e1706b42f9671a05a65e0e
Mohammad Javad Passlar <135098357+MJ-PASSLAR@users.noreply.github.com>
2023-12-27 18:49:38 +0330

    Add files via upload

---
+++ Lesson_6/Code/1DCNN_1DPOOLING_RNN(GRU)_IMDB.py
@@ -0,0 +1,50 @@
+from keras.datasets import imdb
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+max_features = 10000
+max_len = 500
+print('Loading data...')
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
+print('Pad sequences (samples x time)')
+x_train = pad_sequences(x_train, maxlen=max_len)
+x_test = pad_sequences(x_test, maxlen=max_len)
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+
+from keras.models import Sequential
+from keras import layers
+from keras.optimizers import RMSprop
+
+model = Sequential()
+model.add(layers.Conv1D(32, 5, activation='relu',
+                        input_shape=(None, x_train.shape[-1])))
+model.add(layers.MaxPooling1D(3))
+model.add(layers.Conv1D(32, 5, activation='relu'))
+model.add(layers.GRU(32, dropout=0.1, recurrent_dropout=0.5))
+model.add(layers.Dense(1))
+model.summary()
+model.compile(optimizer=RMSprop(), loss='mae')
+history = model.fit(x_train, y_train,
+epochs=10,
+batch_size=128,
+validation_split=0.2)
+
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy') 
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
---
+++ Lesson_6/Code/word Embededding.py
@@ -0,0 +1,41 @@
+from keras.datasets import imdb
+from keras import preprocessing
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+max_features = 10000
+maxlen = 20
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+x_train = pad_sequences(x_train, maxlen=maxlen)
+x_test = pad_sequences(x_test, maxlen=maxlen)
+
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense,Embedding
+
+model = Sequential()
+model.add(Embedding(10000, 8, input_length=maxlen))
+model.add(Flatten())
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+model.summary()
+history = model.fit(x_train, y_train,
+                    epochs=10,
+                    batch_size=32,
+                    validation_split=0.2)
+
+import matplotlib.pyplot as plt
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
